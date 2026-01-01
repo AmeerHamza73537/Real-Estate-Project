5ab8a3289be16ced19270901c3952e505c51bbb2
Ahad Ali <ahadali.reach@gmail.com>
2026-01-01 20:56:17 +0500

    refactor: replace multer with Cloudinary base64 upload and fix auth validation

---
+++ api/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  signup,
+  signin,
+  google,
+  signOut,
+} from "../controller/auth.controller.js";
+
+const router = express.Router();
+router.post("/sign-up", signup);
+router.post("/sign-in", signin);
+router.post("/google", google);
+router.get("/sign-out", signOut);
+
+export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Signin from './pages/SignIn.jsx'
-import SignOut from './pages/SignOut.jsx'
-import Profile from './pages/Profile.jsx'
-import About from './pages/about.jsx'
-import Header from './components/header.jsx'
-import SignUp from './pages/SignUp.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
-import CreateListing from './pages/createListing.jsx'
-import UpdateListing from './pages/UpdateListing.jsx'
-import Listing from './pages/Listing.jsx'
-import Search from './pages/Search.jsx'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Signin from "./pages/SignIn.jsx";
+import SignOut from "./pages/SignOut.jsx";
+import Profile from "./pages/Profile.jsx";
+import About from "./pages/About.jsx";
+import Header from "./components/header.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import CreateListing from "./pages/createListing.jsx";
+import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
+import Search from "./pages/Search.jsx";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/sign-in' element={<Signin/>}/>
-        <Route path='/sign-out' element={<SignOut/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/listing/:listingId' element={<Listing/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route element={<PrivateRoute/>}>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/create-listing' element={<CreateListing/>}/>
-          <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
-
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
